49be44af375cd842e16c87b7fc532aa6eca3aa61
DianWeerasekara <dian@hunome.com>
2026-07-19 18:25:28 +0300

    class validators added

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCategoryDto {}
+import { IsDate, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+
+  @IsString()
+  slug!: string;
+
+  @IsString()
+  description!: string;
+
+  @IsDate()
+  created_date!: Date;
+
+  @IsDate()
+  updated_date!: Date;
+}
